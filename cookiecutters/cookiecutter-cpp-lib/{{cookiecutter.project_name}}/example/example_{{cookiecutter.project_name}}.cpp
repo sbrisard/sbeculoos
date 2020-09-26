9645b131786998028f9b3bfff9f9606879bdde34
Sébastien Brisard <sbrisard@users.noreply.github.com>
2020-09-26 21:32:33 +0200

    Add support for project version

--- cookiecutters/cookiecutter-cpp-lib/{{cookiecutter.project_name}}/example/example_{{cookiecutter.project_name}}.cpp
+++ cookiecutters/cookiecutter-cpp-lib/{{cookiecutter.project_name}}/example/example_{{cookiecutter.project_name}}.cpp
@@ -2,6 +2,8 @@
 #include "{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.hpp"
 
 int main() {
+  std::cout << "version = " << {{cookiecutter.project_name}}::__version__ << std::endl;
+  std::cout << "author = " << {{cookiecutter.project_name}}::__author__ << std::endl;
   std::cout << "return_one() = " << {{cookiecutter.project_name}}::return_one() << std::endl;
   return 0;
 }
