9a468e0f7d49040dff0858c3af111b5cc044b306
Sébastien Brisard <sbrisard@users.noreply.github.com>
2020-10-26 20:48:50 +0100

    Update example (see Issue #3)

--- cookiecutters/cookiecutter-cpp-lib/{{cookiecutter.project_name}}/example/example_{{cookiecutter.project_name}}.cpp
+++ cookiecutters/cookiecutter-cpp-lib/{{cookiecutter.project_name}}/example/example_{{cookiecutter.project_name}}.cpp
@@ -2,8 +2,8 @@
 #include "{{cookiecutter.project_name}}/{{cookiecutter.project_name}}.hpp"
 
 int main() {
-  std::cout << "version = " << {{cookiecutter.project_name}}::__version__ << std::endl;
-  std::cout << "author = " << {{cookiecutter.project_name}}::__author__ << std::endl;
+  std::cout << "version = " << {{cookiecutter.project_name}}::version() << std::endl;
+  std::cout << "author = " << {{cookiecutter.project_name}}::author() << std::endl;
   std::cout << "return_one() = " << {{cookiecutter.project_name}}::return_one() << std::endl;
   return 0;
 }
